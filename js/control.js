97471f4a28c1b9c02bc80658664f3f18c7e25821
bussetjulie <bussetjulie@scm.gforge.inria.fr>
2015-12-14 13:03:44 +0100

    initial version of the project

---
+++ js/control.js
@@ -0,0 +1,17 @@
+/*
+
+var myNameSpace={
+    
+    test: function(){alert('Test');},
+    subNameSpace:{
+        init:function(){
+            this.test();
+        }
+    }
+    
+};
+
+myNameSpace.subNameSpace.init();
+
+*/
+ alert("Azul Fellawen")
